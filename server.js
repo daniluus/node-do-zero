2f8fc64fe643e26739c33b83199b2b46adf7e57a
Danilo Mello <tobeagoofy@gmail.com>
2025-01-03 10:42:35 -0300

    API Node com Postgres

---
+++ server.js
@@ -0,0 +1,54 @@
+import { fastify } from 'fastify'
+import { DatabasePostgres } from './database-postgres.js'
+
+const server = fastify()
+
+const database = new DatabasePostgres()
+
+server.post('/videos', async (request, reply) => {
+
+    const { title, description, duration } = request.body
+
+    await database.create({
+        title: title,
+        description: description,
+        duration: duration
+    })
+    
+    return reply.status(201).send({ message: "Video criado com sucesso!"})
+})
+
+server.get('/videos', async (request, reply) => {
+
+    const search = request.query.search
+
+    const videos = await database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id
+
+    const { title, description, duration } = request.body
+
+    await database.update(videoId, {
+        title: title,
+        description: description,
+        duration: duration
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id
+    
+    await database.delete(videoId)
+
+    return reply.status(204).send({ message: "Video deletado com sucesso!"})
+})
+
+server.listen({
+    port: process.env.PORT ?? 3333,
+})
